b7252daf35e2f966c39c0efa5f942e7bed8258db
kimhansul <gkstmf7506@gmail.com>
2022-03-18 21:31:47 +0900

    슬라이더 플러그인 적용

--- js/index.js
+++ js/index.js
@@ -15,3 +15,40 @@ bt.addEventListener('click', function(e){
     e.preventDefault();
     document.querySelector(bt.getAttribute('href')).scrollIntoView({behavior:'smooth'});
 });
+
+$('.main_notice_content').slick({
+    autoplay: false,
+    dots: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: false,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
